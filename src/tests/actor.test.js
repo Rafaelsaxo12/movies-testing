7f393273a7c5608a1339d83901aa6c8a2e6661bc
Rafael Pérez <rafaelsaxo12@gmail.com>
2024-08-20 14:56:46 -0400

    all actor test

--- src/tests/actor.test.js
+++ src/tests/actor.test.js
@@ -40,7 +40,7 @@ test("Get -> BASE_URL, should return statusCode 200, and res.body.length === 1",
     expect(res.body).toHaveLength(1)
  })
 
-test("Get -> BASE_URL/actorId, should return statusCode 200, and res.body.actor === actor.firstName", async () => { 
+test("Get -> BASE_URL/actorId, should return statusCode 200, and res.body.firstName === actor.firstName", async () => { 
     
     const res = await request(app)
     .get(`${BASE_URL}/${actorId}`)
@@ -55,3 +55,31 @@ test("Get -> BASE_URL/actorId, should return statusCode 200, and res.body.actor 
     expect(res.body.birthday).toBe(actor.birthday)
  })
 
+test("PUT -> BASE_URL/actorId, should return statusCode 200, and res.body.actor === actorUpdate.firstName", async () => {
+    const actorUpdate = {
+        firstName: "Adam",
+        lastName: "Sandler",
+        nationality: "Canadian",
+        image: "AdamImage.png",
+        birthday: "1964-06-17"
+    }
+
+    const res = await request(app)
+    .put(`${BASE_URL}/${actorId}`)
+    .send(actorUpdate)
+    
+    expect(res.status).toBe(200)
+    expect(res.body).toBeDefined()
+    expect(res.body.firstName).toBe(actorUpdate.firstName)
+    expect(res.body.lastName).toBe(actorUpdate.lastName)
+    expect(res.body.nationality).toBe(actorUpdate.nationality)
+    expect(res.body.image).toBe(actorUpdate.image)
+    expect(res.body.birthday).toBe(actorUpdate.birthday)
+})
+
+test("DELETE -> BASE_URL/actorId, should return statusCode 204", async () => {
+    const res = await request(app)
+    .delete(`${BASE_URL}/${actorId}`)
+
+    expect(res.statusCode).toBe(204)
+})
